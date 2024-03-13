ce16ba01e07a9b477c56f71288f58442a833f785
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2024-03-13 14:28:04 -0300

    Add files via upload

---
+++ syncOCS.ps1
@@ -0,0 +1,11 @@
+$exePath = "C:\Program Files\OCS Inventory Agent\OCSInventory.exe"
+
+$action = New-ScheduledTaskAction -Execute $exePath
+
+$trigger1 = New-ScheduledTaskTrigger -At 9am -Daily
+$trigger2 = New-ScheduledTaskTrigger -At 12pm -Daily
+$trigger3 = New-ScheduledTaskTrigger -At 3pm -Daily
+
+$principal = New-ScheduledTaskPrincipal -UserId "Autoridade NT\SISTEMA" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask -Action $action -Trigger $trigger1, $trigger2, $trigger3 -TaskName "SyncOCS" -Principal $principal
