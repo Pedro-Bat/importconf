e26d08f5d8f2d334625448c4e421358856fad2d5
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2024-03-13 14:54:19 -0300

    Update syncOCS.ps1

--- syncOCS.ps1
+++ syncOCS.ps1
@@ -1,7 +1,9 @@
 $exePath = "C:\Program Files\OCS Inventory Agent\OCSInventory.exe"
 $action = New-ScheduledTaskAction -Execute $exePath
 $trigger1 = New-ScheduledTaskTrigger -At 9am -Daily
+$trigger4 = New-ScheduledTaskTrigger -At 11am -Daily
 $trigger2 = New-ScheduledTaskTrigger -At 12pm -Daily
 $trigger3 = New-ScheduledTaskTrigger -At 3pm -Daily
+$trigger5 = New-ScheduledTaskTrigger -At 1pm -Daily
 $principal = New-ScheduledTaskPrincipal -UserId "Autoridade NT\SISTEMA" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -Action $action -Trigger $trigger1, $trigger2, $trigger3 -TaskName "SyncOCS" -Principal $principal
+Register-ScheduledTask -Action $action -Trigger $trigger1, $trigger2, $trigger3, $trigger4, $trigger5 -TaskName "SyncOCS" -Principal $principal
