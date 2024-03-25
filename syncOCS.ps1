bcf123ee4691fa58b204284a94e038b6eef7665d
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2024-03-25 18:59:26 -0300

    Update syncOCS.ps1

--- syncOCS.ps1
+++ syncOCS.ps1
@@ -5,5 +5,7 @@ $trigger4 = New-ScheduledTaskTrigger -At 11am -Daily
 $trigger2 = New-ScheduledTaskTrigger -At 12pm -Daily
 $trigger3 = New-ScheduledTaskTrigger -At 3pm -Daily
 $trigger5 = New-ScheduledTaskTrigger -At 1pm -Daily
+$trigger6 = New-ScheduledTaskTrigger -At 4pm -Daily
+$trigger7 = New-ScheduledTaskTrigger -At 5pm -Daily
 $principal = New-ScheduledTaskPrincipal -UserId "Autoridade NT\SISTEMA" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -Action $action -Trigger $trigger1, $trigger2, $trigger3, $trigger4, $trigger5 -TaskName "SyncOCS" -Principal $principal
+Register-ScheduledTask -Action $action -Trigger $trigger1, $trigger2, $trigger3, $trigger4, $trigger5, $trigger6, $trigger7 -TaskName "SyncOCS" -Principal $principal
