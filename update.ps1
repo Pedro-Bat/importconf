d9073fc5fded2794d404f2b13842614f3b6e0b4e
Pedro-Bat <104031353+Pedro-Bat@users.noreply.github.com>
2024-06-24 15:27:20 -0300

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,7 +1,2 @@
 Get-ScheduledTask | Where-Object{$_.taskname -match "GoogleUpdater"} | Start-ScheduledTask
 Get-ScheduledTask | Where-Object{$_.taskname -match "Firefox Background Update"} | Start-ScheduledTask
-install-packageprovider -name nuget -force
-Install-Module PsWindowsUpdate -force
-Import-Module PSWindowsUpdate
-Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
-pause
